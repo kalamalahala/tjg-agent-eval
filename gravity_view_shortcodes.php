17cb322ddf85eb8a2dc0791a849d8c3528ee6978
kalamalahala <solo.driver.bob@gmail.com>
2022-01-03 18:07:50 -0500

    new headers, started gravityview

---
+++ gravity_view_shortcodes.php
@@ -0,0 +1,9 @@
+<?php
+
+add_shortcode( 'display_training_results', 'display_gv7126' );
+
+function display_gv7126 ( $atts ) {
+    // [gravityview id="7126" search_field="" search_operator="" search_value="" filter=""];
+}
+
+?>
--- header-links.php
+++ header-links.php
@@ -2,8 +2,10 @@
 
 add_shortcode ( 'dashboard_header', 'dashboard__header' );
 
-function dashboard__header () {
-    
+function dashboard__header ( $atts ) {
+
+    $headerProperties = shortcode_atts( array('mode' => ''), $atts, 'dashboard_header' );
+
     // Check for agent_id parameter, get user's ID from WP User object. If not provided, use the currently logged in user.
     if (!isset($_GET['agent_id'])) {
         $userToDisplay = get_current_user_id();
@@ -25,13 +27,23 @@ function dashboard__header () {
         'agent_position' => get_user_meta($userToDisplay, 'agent_position', true)
     );
 
-    // begin HTML layout
-    $page__title = '<div class="pageTitle">Leadership Dashboard</div>';
-    $dashboard__name__and__title = '<div class="nameAndTitle">' . $dashboard__user['first_name'] . ' ' . $dashboard__user['last_name'] . ' - ' . $dashboard__user['agent_position'] . '</div>';
-    $page__subtitle = '<div class="pageSubtitle">Viewing ' . $dashboard__user['first_name'] . '\'s Team</div>';
-    $name__title__etc = $dashboard__name__and__title . $page__subtitle;
+    // begin HTML layouts
 
-    $description__box = '<div class="descriptionBox">' . $name__title__etc . '</div>';
+    switch ( $headerProperties['mode'] ) {
+        case 'single':
+            $page__title = '<div class="pageTitle">Presentation Training</div>';
+            $dashboard__name__and__title = '<div class="nameAndTitle">' . $dashboard__user['first_name'] . ' ' . $dashboard__user['last_name'] . ' - ' . $dashboard__user['agent_position'] . '</div>';
+            $description__box = '<div class="descriptionBox">' . $dashboard__name__and__title . '</div>';
+            break;
+
+        default:
+        $page__title = '<div class="pageTitle">Leadership Dashboard</div>';
+        $dashboard__name__and__title = '<div class="nameAndTitle">' . $dashboard__user['first_name'] . ' ' . $dashboard__user['last_name'] . ' - ' . $dashboard__user['agent_position'] . '</div>';
+        $page__subtitle = '<div class="pageSubtitle">Viewing ' . $dashboard__user['first_name'] . '\'s Team</div>';
+        $name__title__etc = $dashboard__name__and__title . $page__subtitle;
+        $description__box = '<div class="descriptionBox">' . $name__title__etc . '</div>';
+            break;
+    }
 
     return $page__title . $description__box;
 
