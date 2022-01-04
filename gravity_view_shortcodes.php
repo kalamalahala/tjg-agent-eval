ad40537930e031854889060f410dd94117508c36
kalamalahala <solo.driver.bob@gmail.com>
2022-01-04 16:02:23 -0500

    updated gv_change_back_link

--- gravity_view_shortcodes.php
+++ gravity_view_shortcodes.php
@@ -38,4 +38,31 @@ function display_gv7126 ( $atts ) {
 
 }
 
+add_action( 'gravityview/template/links/back/url', 'gv_change_back_link', 10, 2 );
+
+/**
+ * Modify the back link URL for a specific View
+ * 
+ * @param string $href The original back link
+ * @param \GV\Template_Context The current context
+ *
+ * @return string Modified URL, if View ID equals 7126
+ */
+function gv_change_back_link( $href = '', $context = null ) {
+    
+    // GitHub snippet to get full URI, parameters, and anchor
+    $base_url = ( isset($_SERVER['HTTPS']) && $_SERVER['HTTPS']=='on' ? 'https' : 'http' ) . '://' .  $_SERVER['HTTP_HOST'];
+    $url = $base_url . $_SERVER["REQUEST_URI"];
+
+    // parse_url with PHP_URL_QUERY to return all parameters as a string, append them to a question mark
+    $parameters = '?' . parse_url($url, PHP_URL_QUERY);
+
+	if( 7126 === $context->view->ID ) {
+        // Use original Go Back link with appended parameters
+		return $href . $parameters;
+	}
+	
+	return $href;
+}
+
 ?>
